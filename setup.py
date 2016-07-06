e26a5fb51e3da24843699614de8580588ac4b036
UmutOrman <umutormans@gmail.com>
2016-07-06 21:02:23 +0300

    Example pip installable project by PeterDowns

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ setup(
   description = 'A random test lib',
   author = 'Peter Downs',
   author_email = 'peterldowns@gmail.com',
-  url = 'https://github.com/peterldowns/mypackage', # use the URL to the github repo
-  download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1', # I'll explain this in a second
+  url = 'https://github.com/UmutOrman/mypackage', # use the URL to the github repo
+  download_url = 'https://github.com/UmutOrman/mypackage/tarball/0.1', # I'll explain this in a second
   keywords = ['testing', 'logging', 'example'], # arbitrary keywords
   classifiers = [],
 )
