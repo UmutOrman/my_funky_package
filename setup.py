9721c309237b8a7add317a583e2d292785d0b570
UmutOrman <umutormans@gmail.com>
2016-07-06 21:26:12 +0300

    Example pip installable project by PeterDowns

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 
 setup(
-  name = 'mypackage',
-  packages = ['mypackage'], # this must be the same as the name above #???
+  name = 'my_funky_package',
+  packages = ['my_funky_package'], # this must be the same as the name above #???
   version = '0.1',
   description = 'A random test lib',
-  author = 'Peter Downs',
-  author_email = 'peterldowns@gmail.com',
+  author = 'Umut Orman',
+  author_email = 'umutormans@gmail.com',
   url = 'https://github.com/UmutOrman/mypackage', # use the URL to the github repo
   download_url = 'https://github.com/UmutOrman/mypackage/tarball/0.1', # I'll explain this in a second
   keywords = ['testing', 'logging', 'example'], # arbitrary keywords
